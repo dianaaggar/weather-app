e9cae2141eccde4625f4a33e4bffb6e2e255719a
dianaaggar <57739072+dianaaggar@users.noreply.github.com>
2023-07-25 15:25:44 -0400

    weather app api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+
+
+// function API code 2 different ways 
+// 1st way
+// const url = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=f765478da97b8124bef0c9d2cb6bcf12'
+
+// or 2nd way define api object / base url is going to be this url that we are going to fetch from
+const api = {
+  key: "f765478da97b8124bef0c9d2cb6bcf12",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+// Function to display temperature in Fahrenheit
+const TemperatureFahrenheit = ({ temperature }) => {
+  return <p>{temperature}°F</p>;
+};
 
 function App() {
+
+  // hold state of search term
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState({});
+  // store weather json object inside of our app/ as a state variable-usestate
+
+  const searchPressed = () => {
+    // console.log (searchPressed);
+    console.log(search);
+
+// fetch and get data from url
+    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+    .then((response) => response.json())
+    .then((result) => {
+    setData(result);
+
+    console.log(result);
+    })
+    // handle the error 
+    .catch((error) => {
+    console.error("Error 404", error);
+    });
+  };
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Weather</h1>
+
+    {/* Search Box */}
+      <div>
+        <input
+          type="text"
+          placeholder="Enter city"
+          onChange={(e) => setSearch(e.target.value)}
+          // when search change update search term 
+        />
+
+        <button onClick={searchPressed}>Search</button>
+      </div>
+{/* when reload check the weather.main is not equal undefined */}
+      {/* Display weather information */}
+      {data && data.main && data.weather ? (
+        <div>
+          {/* Location */}
+          <p>{data.name}</p>
+
+          {/* Temperature Fahrenheit */}
+          <TemperatureFahrenheit
+            temperature={(data.main.temp* 9) / 5 + 32}
+          />
+          <p>{data.main.humidity}%rh</p>
+
+          {/* Weather Condition */}
+          <p>{data.weather[0].main}</p>
+          <p>{data.weather[0].description}</p>
+        </div>
+      ) 
+
+      :(" ")}
     </div>
+
+
   );
 }
 
 export default App;
+
